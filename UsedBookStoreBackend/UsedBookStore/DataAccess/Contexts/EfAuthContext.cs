08c159451d0604f218b92c75cbd3d57eba32070f
Nguyễn Quốc Vương <boide03@gmail.com>
2023-11-14 21:12:41 +0700

    seeding roles

--- UsedBookStoreBackend/UsedBookStore/DataAccess/Contexts/EfAuthContext.cs
+++ UsedBookStoreBackend/UsedBookStore/DataAccess/Contexts/EfAuthContext.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,37 @@ namespace UsedBookStore.DataAccess.Contexts
     {
         public EfAuthContext(DbContextOptions<EfAuthContext> options) : base(options)
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "8db0a420-2468-49b4-ad4b-cf99dc4984ea";
+            var writerRoleId = "aeeaa11e-1e69-44d1-adfd-4014f3e6cade";
+
+            var roles = new List<IdentityRole>
+            { 
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                 new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                },
+            };
+
+            // we now have to seed this inside the builder object over here
+            builder.Entity<IdentityRole>().HasData(roles);
+            // so when we run entity framwork core migrations, see this data inject
 
         }
     }
